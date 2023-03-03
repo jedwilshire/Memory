bb4d6658a6865dfea324f8f5a68333b50ee6c278
jedwilshire <jedwilshire@gmail.com>
2023-03-03 17:07:53 -0600

    Changed from pygame-zero to pygame only

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pgzrun
 from settings import *
 from sprites import *
+import pygame
 from random import shuffle
 firstCard = None
 secondCard = None
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 def make_board():
     board_maker = []
     for shape in ALLSHAPES:
@@ -27,7 +28,8 @@ def draw():
         for col in range(BOARDWIDTH):
             card = board[row][col]
             card.draw(screen)
-            
+    pygame.display.flip()
+    
 def on_mouse_down(pos):
     global firstCard, secondCard
     for y in range(BOARDHEIGHT):
@@ -73,9 +75,28 @@ def makeList(row, col):
         for k in range(col):
             board[i].append(None)
     return board
+
+def mainloop():
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+            # event loop
+            update()
+            draw()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+                elif event.type == pygame.MOUSEMOTION:
+                    on_mouse_move(event.pos)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    on_mouse_down(event.pos)
+            clock.tick(FPS)
+            
 board = makeList(BOARDHEIGHT, BOARDWIDTH)
 make_board()
-pgzrun.go()
+pygame.init()
+mainloop()
 
 
 
--- sprites.py
+++ sprites.py
@@ -1,4 +1,5 @@
-from pygame import Rect
+from pygame import Rect, Surface
+import pygame
 from settings import *
 class Card:
     def __init__(self, x, y, shape, color):
@@ -9,48 +10,51 @@ class Card:
         self.highlighted = False
         self.timer = 0
         self.matched = False
+        self.back = self.makeBack()
         
+    def makeBack(self):
+        back = Surface((BOXSIZE, BOXSIZE))
+        back.fill(LIGHTBGCOLOR)
+        shape = self.shape
+        color = self.color
+        x = 0
+        y = 0
+        if shape == DONUT:
+            cx = x + BOXSIZE // 2
+            cy = y + BOXSIZE // 2
+            pygame.draw.circle(back, color, (cx, cy), BOXSIZE * .40)
+            pygame.draw.circle(back, LIGHTBGCOLOR, (cx, cy), BOXSIZE * .20)
+        elif shape == SQUARE:
+            rect = Rect(x + BOXSIZE * .2, y + BOXSIZE * .2, BOXSIZE * .6, BOXSIZE * .6)
+            pygame.draw.rect(back, color, rect)
+        elif shape == CROSS:
+            pygame.draw.line(back, color, (x, y + BOXSIZE // 2),
+                             (x + BOXSIZE, y + BOXSIZE // 2))
+            pygame.draw.line(back, color, (x + BOXSIZE // 2, y),
+                             (x + BOXSIZE // 2, y + BOXSIZE))
+        elif shape == LINES:
+            numLines = 10
+            gap = BOXSIZE // numLines
+            for i in range(1, numLines, 1):
+                pygame.draw.line(back, color, (x + gap * i, y),
+                                 (x + gap * i, y + BOXSIZE))
+        elif shape == DIAMOND:
+            pygame.draw.line(back, color, (x + BOXSIZE // 2, y),
+                             (x + BOXSIZE, y + BOXSIZE // 2))
+            pygame.draw.line(back, color, (x + BOXSIZE, y + BOXSIZE // 2),
+                             (x + BOXSIZE // 2, y + BOXSIZE))
+            pygame.draw.line(back, color, (x + BOXSIZE // 2, y + BOXSIZE),
+                             (x, y + BOXSIZE // 2))
+            pygame.draw.line(back, color, (x, y + BOXSIZE // 2),
+                             (x + BOXSIZE // 2, y))
+        return back
+    
     def draw(self, screen):
         if self.turnedUp == False:
             if not self.highlighted:
-                screen.draw.filled_rect(self.rect, BOXCOLOR)
+                pygame.draw.rect(screen, BOXCOLOR, self.rect)
             else:
-                screen.draw.filled_rect(self.rect, HIGHLIGHTCOLOR)
+                pygame.draw.rect(screen, HIGHLIGHTCOLOR, self.rect)
         else:
-            screen.draw.filled_rect(self.rect, LIGHTBGCOLOR)
-            shape = self.shape
-            color = self.color
-            x = self.rect.x
-            y = self.rect.y
-            if shape == DONUT:
-                cx = x + BOXSIZE // 2
-                cy = y + BOXSIZE // 2
-                screen.draw.filled_circle((cx, cy), BOXSIZE * .40, color)
-                screen.draw.filled_circle((cx, cy), BOXSIZE * .20, LIGHTBGCOLOR)
-            elif shape == SQUARE:
-                rect = Rect(x + BOXSIZE * .2, y + BOXSIZE * .2, BOXSIZE * .6, BOXSIZE * .6)
-                screen.draw.filled_rect(rect, color)
-            elif shape == CROSS:
-                screen.draw.line((x, y + BOXSIZE // 2),
-                                 (x + BOXSIZE, y + BOXSIZE // 2), color)
-                screen.draw.line((x + BOXSIZE // 2, y),
-                                 (x + BOXSIZE // 2, y + BOXSIZE), color)
-            elif shape == LINES:
-                numLines = 10
-                gap = BOXSIZE // numLines
-                for i in range(1, numLines, 1):
-                    screen.draw.line((x + gap * i, y),
-                                     (x + gap * i, y + BOXSIZE), color)
-            elif shape == DIAMOND:
-                screen.draw.line((x + BOXSIZE // 2, y),
-                                 (x + BOXSIZE, y + BOXSIZE // 2),
-                                 color)
-                screen.draw.line((x + BOXSIZE, y + BOXSIZE // 2),
-                                 (x + BOXSIZE // 2, y + BOXSIZE),
-                                 color)
-                screen.draw.line((x + BOXSIZE // 2, y + BOXSIZE),
-                                 (x, y + BOXSIZE // 2),
-                                 color)
-                screen.draw.line((x, y + BOXSIZE // 2),
-                                 (x + BOXSIZE // 2, y),
-                                 color)
+            screen.blit(self.back, self.rect)
+            
