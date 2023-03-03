3992e6d6d2b0e7e34a64baaeb60425e95a488054
jedwilshire <jedwilshire@gmail.com>
2023-03-03 13:49:19 -0600

    trying to implement without matrix

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ import pgzrun
 from settings import *
 from random import shuffle
 board = []
-
+selected = [-1, -1]
 def make_board():
     for shape in ALLSHAPES:
         for color in ALLCOLORS:
-            if shape == SQUARE:
-                board.append([shape, color, True])
-                board.append([shape, color, True])
-            else:
-                board.append([shape, color, False])
-                board.append([shape, color, False])
+            board.append([shape, color, False])
+            board.append([shape, color, False])
     shuffle(board)
+    
 def draw():
     screen.fill(BGCOLOR)
     index = 0
@@ -36,7 +33,30 @@ def draw():
                 elif shape == SQUARE:
                     rect = Rect(x + BOXSIZE * .2, y + BOXSIZE * .2, BOXSIZE * .6, BOXSIZE * .6)
                     screen.draw.filled_rect(rect, color)
-                
+                elif shape == CROSS:
+                    screen.draw.line((x, y + BOXSIZE // 2),
+                                     (x + BOXSIZE, y + BOXSIZE // 2), color)
+                    screen.draw.line((x + BOXSIZE // 2, y),
+                                     (x + BOXSIZE // 2, y + BOXSIZE), color)
+                elif shape == LINES:
+                    numLines = 10
+                    gap = BOXSIZE // numLines
+                    for i in range(1, numLines, 1):
+                        screen.draw.line((x + gap * i, y),
+                                         (x + gap * i, y + BOXSIZE), color)
+                elif shape == DIAMOND:
+                    screen.draw.line((x + BOXSIZE // 2, y),
+                                     (x + BOXSIZE, y + BOXSIZE // 2),
+                                     color)
+                    screen.draw.line((x + BOXSIZE, y + BOXSIZE // 2),
+                                     (x + BOXSIZE // 2, y + BOXSIZE),
+                                     color)
+                    screen.draw.line((x + BOXSIZE // 2, y + BOXSIZE),
+                                     (x, y + BOXSIZE // 2),
+                                     color)
+                    screen.draw.line((x, y + BOXSIZE // 2),
+                                     (x + BOXSIZE // 2, y),
+                                     color)
             x += BOXSIZE + GAPSIZE
             index += 1
         y += BOXSIZE + GAPSIZE
@@ -44,6 +64,28 @@ def draw():
        
 def update():
     pass
+def on_mouse_down(pos):
+    x = (pos[0] - GAPSIZE) // (BOXSIZE + GAPSIZE)
+    y = (pos[1] - GAPSIZE) // (BOXSIZE + GAPSIZE)
+    if (x >= 0 and y >= 0 and selected[0] == -1):
+        board[y * BOARDWIDTH + x][2] = True 
+        selected[0] = x
+        selected[1] = y
+    elif (x >= 0 and y >= 0):
+        board[y * BOARDWIDTH + x][2] = True
+        previous_shape = board[selected[1] * BOARDWIDTH + selected[0]][0]
+        previous_color = board[selected[1] * BOARDWIDTH + selected[0]][1]
+        shape = board[y * BOARDWIDTH + x][0]
+        color = board[y * BOARDWIDTH + x][1]
+        if previous_shape != shape or previous_color != color:
+            #add animation
+            
+            board[y * BOARDWIDTH + x][2] = False
+            board[selected[1] * BOARDWIDTH + selected[0]][2] = False
+        selected[0] = -1
+        selected[1] = -1
+            
+            
 make_board()
 pgzrun.go()
 
