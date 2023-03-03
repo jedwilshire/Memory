121aa24b95eecb3d651d426f68ef41a9f3dbc6ba
jedwilshire <jedwilshire@gmail.com>
2023-03-03 16:22:30 -0600

    completed matching game
    Matching game completed using a matrix and pygame zero

--- main.py
+++ main.py
@@ -1,91 +1,79 @@
 import pgzrun
 from settings import *
+from sprites import *
 from random import shuffle
-board = []
-selected = [-1, -1]
+firstCard = None
+secondCard = None
 def make_board():
+    board_maker = []
     for shape in ALLSHAPES:
         for color in ALLCOLORS:
-            board.append([shape, color, False])
-            board.append([shape, color, False])
-    shuffle(board)
+            board_maker.append([shape, color])
+            board_maker.append([shape, color])
+    shuffle(board_maker)
+    index = 0
+    for row in range(BOARDHEIGHT):
+        for col in range(BOARDWIDTH):
+            x = GAP + col * (GAP + BOXSIZE)
+            y = GAP + row * (GAP + BOXSIZE)
+            shape = board_maker[index][0]
+            color = board_maker[index][1]
+            board[row][col] = Card(x, y, shape, color)
+            index += 1
     
 def draw():
     screen.fill(BGCOLOR)
-    index = 0
-    x = GAPSIZE
-    y = GAPSIZE
     for row in range(BOARDHEIGHT):
         for col in range(BOARDWIDTH):
-            if board[index][2] == False:
-                rect = Rect(x, y, BOXSIZE, BOXSIZE)
-                screen.draw.filled_rect(rect, BOXCOLOR)
-            else:
-                rect = Rect(x, y, BOXSIZE, BOXSIZE)
-                screen.draw.filled_rect(rect, LIGHTBGCOLOR)
-                shape = board[index][0]
-                color = board[index][1]
-                if shape == DONUT:
-                    cx = x + BOXSIZE // 2
-                    cy = y + BOXSIZE // 2
-                    screen.draw.filled_circle((cx, cy), BOXSIZE * .40, color)
-                    screen.draw.filled_circle((cx, cy), BOXSIZE * .20, LIGHTBGCOLOR)
-                elif shape == SQUARE:
-                    rect = Rect(x + BOXSIZE * .2, y + BOXSIZE * .2, BOXSIZE * .6, BOXSIZE * .6)
-                    screen.draw.filled_rect(rect, color)
-                elif shape == CROSS:
-                    screen.draw.line((x, y + BOXSIZE // 2),
-                                     (x + BOXSIZE, y + BOXSIZE // 2), color)
-                    screen.draw.line((x + BOXSIZE // 2, y),
-                                     (x + BOXSIZE // 2, y + BOXSIZE), color)
-                elif shape == LINES:
-                    numLines = 10
-                    gap = BOXSIZE // numLines
-                    for i in range(1, numLines, 1):
-                        screen.draw.line((x + gap * i, y),
-                                         (x + gap * i, y + BOXSIZE), color)
-                elif shape == DIAMOND:
-                    screen.draw.line((x + BOXSIZE // 2, y),
-                                     (x + BOXSIZE, y + BOXSIZE // 2),
-                                     color)
-                    screen.draw.line((x + BOXSIZE, y + BOXSIZE // 2),
-                                     (x + BOXSIZE // 2, y + BOXSIZE),
-                                     color)
-                    screen.draw.line((x + BOXSIZE // 2, y + BOXSIZE),
-                                     (x, y + BOXSIZE // 2),
-                                     color)
-                    screen.draw.line((x, y + BOXSIZE // 2),
-                                     (x + BOXSIZE // 2, y),
-                                     color)
-            x += BOXSIZE + GAPSIZE
-            index += 1
-        y += BOXSIZE + GAPSIZE
-        x = GAPSIZE
-       
-def update():
-    pass
+            card = board[row][col]
+            card.draw(screen)
+            
 def on_mouse_down(pos):
-    x = (pos[0] - GAPSIZE) // (BOXSIZE + GAPSIZE)
-    y = (pos[1] - GAPSIZE) // (BOXSIZE + GAPSIZE)
-    if (x >= 0 and y >= 0 and selected[0] == -1):
-        board[y * BOARDWIDTH + x][2] = True 
-        selected[0] = x
-        selected[1] = y
-    elif (x >= 0 and y >= 0):
-        board[y * BOARDWIDTH + x][2] = True
-        previous_shape = board[selected[1] * BOARDWIDTH + selected[0]][0]
-        previous_color = board[selected[1] * BOARDWIDTH + selected[0]][1]
-        shape = board[y * BOARDWIDTH + x][0]
-        color = board[y * BOARDWIDTH + x][1]
-        if previous_shape != shape or previous_color != color:
-            #add animation
+    global firstCard, secondCard
+    for y in range(BOARDHEIGHT):
+        for x in range(BOARDWIDTH):
+            card = board[y][x]
+            if card.rect.collidepoint(pos) and card != firstCard:
+                card.turnedUp = True
+                if firstCard == None:
+                    firstCard = card
+                else:
+                    secondCard = card
+                    if (firstCard.shape != secondCard.shape or
+                        firstCard.color != secondCard.color):
+                        firstCard.timer = 60
+                        secondCard.timer = 60
+                    else:
+                        firstCard.matched = True
+                        secondCard.matched = True
+                    firstCard = None
+                    secondCard = None
+                    
+def on_mouse_move(pos):
+    for y in range(BOARDHEIGHT):
+        for x in range(BOARDWIDTH):
+            card = board[y][x]
+            if card.rect.collidepoint(pos):
+                card.highlighted = True
+            else:
+                card.highlighted = False
+def update():
+    for y in range(BOARDHEIGHT):
+        for x in range(BOARDWIDTH):
+            card = board[y][x]
+            if card.timer > 0:
+                card.timer -= 1
+            elif not card.matched and not card == firstCard:
+                card.turnedUp = False
             
-            board[y * BOARDWIDTH + x][2] = False
-            board[selected[1] * BOARDWIDTH + selected[0]][2] = False
-        selected[0] = -1
-        selected[1] = -1
-            
-            
+def makeList(row, col):
+    board = []
+    for i in range(row):
+        board.append([])
+        for k in range(col):
+            board[i].append(None)
+    return board
+board = makeList(BOARDHEIGHT, BOARDWIDTH)
 make_board()
 pgzrun.go()
 
